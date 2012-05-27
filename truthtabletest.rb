b43a8bb260f5227d2041b9e0bcd55ff3433b83bf
Matthias Beyer <matthias@ib-fb.de>
2012-05-27 12:22:03 +0200

    added truthtests

--- truthtabletest.rb
+++ truthtabletest.rb
@@ -1,6 +1,7 @@
+TRUE_STATE  = 1
+FALSE_STATE = 0
 
-
-# TruthTable for maximal 2**4 States
+# TruthTable for maximal 2**4 States, (MemoryError when 2**5 on my machine)
 class TruthTable
 
   attr_accessor :states
@@ -18,8 +19,9 @@ class TruthTable
       new = Array.new
       until new.length == ( 2**count )
         t = count**2 / factor
-        t.times do new << false end
-        t.times do new << true end
+        t = 1 if t == 0
+        t.times do new << FALSE_STATE end
+        t.times do new << TRUE_STATE end
       end
       
       ary << new
@@ -31,11 +33,72 @@ class TruthTable
   def to_s
     0.upto @states.first.length-1 do |i|
       @states.each do |ary|
-        bit = ary[i] ? 1.to_s : 0.to_s
-        print bit + " "
+        print ary[i].to_s + " "
       end
       print $/
     end
   end
 
+  # overwrite attr_reader
+  def states
+    all_states = Array.new
+    0.upto @states.first.length - 1 do |i|
+      ary = Array.new
+      @states.each do |state|
+        ary << state[i]
+      end
+      all_states << ary
+    end
+    all_states
+  end
+
+  def each_state &block
+    states.each &block
+  end
+
+  def each_state_with_index &block
+    states.each_with_index &block
+  end
+
+  def each_state_with_object &block
+    states.each_with_object &block
+  end
+
+end
+
+require 'test/unit'
+load 'blackboxes.rb'
+
+class TruthTableTest < Test::Unit::TestCase
+
+  def test_and
+    should = [ false, false, false, true ]
+    do_test should, 2, :AND 
+  end
+
+  def test_not
+    should = [ true, false ]
+    do_test should, 1, :NOT
+  end
+
+  def test_yes
+    should = [ false, true ]
+    do_test should, 1, :YES
+  end 
+
+  def test_or
+    should = [ false, true, true, true ]
+    do_test should, 2, :OR
+  end
+
+  def test_xor
+    should = [ false, true, true, false ]
+    do_test should, 2, :XOR
+  end
+
+  def do_test( should, tt_len, box_sym )
+    TruthTable.new( tt_len ).each_state_with_index do |state, i|
+      assert_equal( should[i], Kernel.send( box_sym, *state ).on(0) )
+    end
+  end
 end
